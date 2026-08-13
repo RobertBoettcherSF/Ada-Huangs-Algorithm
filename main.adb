ab4beb5787c8d58ba4df797123a4df3d6a2831b0
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-13 21:07:01 +0200

    Create main.adb

---
+++ main.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("=====================================");
+   Put_Line (" Huang's Termination Detection Alg");
+   Put_Line ("=====================================");
+   Put_Line ("System compiled successfully.");
+   Put_Line ("To run the V&V suite: 'make test'");
+end Main;
